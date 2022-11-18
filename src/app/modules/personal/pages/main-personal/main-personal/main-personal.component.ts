4a2f98e4b61d8cbe51507cdab01242e3fd1dc7b1
JosafatMV <20213tn124@utez.edu.mx>
2022-11-18 12:20:10 -0600

    Table styled

--- src/app/modules/personal/pages/main-personal/main-personal/main-personal.component.ts
+++ src/app/modules/personal/pages/main-personal/main-personal/main-personal.component.ts
@@ -4,37 +4,38 @@ import { PersonalService } from '../../../service/personal.service';
 import { GeneralService } from '../../../../../services/general.service';
 
 @Component({
-	selector: 'app-main-personal',
-	templateUrl: './main-personal.component.html',
+  selector: 'app-main-personal',
+  templateUrl: './main-personal.component.html',
 })
 export class MainPersonalComponent {
-	PERSONAL_DATA: Personal[] = [];
+  PERSONAL_DATA: Personal[] = [];
 
-	displayedColumns: string[] = [
-		'id',
-		'nombre',
-		'apellidoP',
-		'apellidoM',
-		'puesto',
-		'sueldo',
-		'fecNac',
-	];
-	dataSource = this.PERSONAL_DATA;
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'surname',
+    'lastname',
+    'position',
+    'salary',
+    'birthday',
+  ];
+  dataSource = this.PERSONAL_DATA;
 
-	constructor(
-		private personalService: PersonalService,
-		private generalService: GeneralService
-	) {
-		this.getPersonal();
-	}
+  constructor(
+    private personalService: PersonalService,
+    private generalService: GeneralService
+  ) {
+    this.getPersonal();
+  }
 
-	getPersonal() {
-		console.log(this.generalService.token);
+  getPersonal() {
+    console.log(this.generalService.token);
 
-		this.personalService.getPersonal().subscribe((resp) => {
-			this.PERSONAL_DATA = resp;
-			this.dataSource = this.PERSONAL_DATA;
-			this.PERSONAL_DATA;
-		});
-	}
+    this.personalService.getPersonal().subscribe((resp) => {
+      this.PERSONAL_DATA = resp;
+      this.dataSource = this.PERSONAL_DATA;
+      this.PERSONAL_DATA;
+    });
+  }
+
 }
