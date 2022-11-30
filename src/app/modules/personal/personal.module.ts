e5c205265c874a08790a5ba48ad4f15b06630586
Josamv <novairesh@gmail.com>
2022-11-30 08:14:48 -0600

    add form semi acabado

--- src/app/modules/personal/personal.module.ts
+++ src/app/modules/personal/personal.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainPersonalComponent } from './pages/main-personal/main-personal/main-personal.component';
 import { materialModules } from 'src/app/types/material-modules';
 import { AddPersonalComponent } from './pages/add-personal/add-personal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [MainPersonalComponent, AddPersonalComponent],
-  imports: [CommonModule, ...materialModules],
+	declarations: [MainPersonalComponent, AddPersonalComponent],
+	imports: [CommonModule, FormsModule, ...materialModules],
+	bootstrap: [MainPersonalComponent],
 })
 export class PersonalModule {}
